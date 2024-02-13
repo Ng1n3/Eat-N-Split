1ff68c9c52a82fab8d3cbb9d4585676142ece2e6
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-13 12:51:42 +0100

    static part complete

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ export default function App() {
         <FormAddFriend/>
         <Button>Add friend</Button>
       </div>
+      <FormSplitBill/>
     </div>
   );
 }
@@ -84,4 +85,25 @@ function FormAddFriend() {
 
 }
 
-function FormSplitBill
+function FormSplitBill() {
+  return <form className="form-split-bill">
+    <h2>Split a bill with X </h2>
+
+    <label> 💰 Bill Value</label>
+    <input type="text"/>
+    
+    <label> 👩‍🦱 Your expense</label>
+    <input type="text"/>
+    
+    <label> 🧑‍🤝‍🧑X's expense</label>
+    <input type="text" disabled/>
+
+    <label> 🤑 Who is paying the bill</label>
+    <select>
+      <option value="user">You</option>
+      <option value="friend">X</option>
+    </select>
+
+    <Button>Split bill</Button>
+  </form>
+}
